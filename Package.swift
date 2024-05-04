a611356cd69b58979dd424291a4b5ff024c844a7
Artur  Mkrtchyan <mkrtarturdev@gmail.com>
2024-05-05 02:08:49 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-let package = Package(
-    name: "UnityAds",
-	platforms: [.iOS(.v15)],
-    products: [
-        .library(name: "UnityAds", targets: ["UnityAds"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "UnityAds",
-            dependencies: [],
-			path: "SourceCode"),
-    ],
-	swiftLanguageVersions: [.v5]
+import PackageDescription
+
+let package = Package(name: "UnityAds",
+		      platforms: [.iOS(.v15)],
+		      products: [
+			.library(name: "UnityAds", targets: ["UnityAds"]),
+		      ],
+		      targets: [
+			.target(name: "UnityAds",
+				path: "SourceCode",
+			       	exclude: ["Info.plist"],
+				resources: []),
+		      ],
+		      swiftLanguageVersions: [.v5]
 )
